005d4946e81ab2f3435944117da4e50ef4317a58
Abdullah Ercan <abdullahercan@abdullah-imac.local>
2019-12-01 15:53:31 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './Style.css';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://randomuser.me/api/")
+      .then(res => res.json())
+      .then(
+        (e) => {
+          this.setState({
+            loading: false,
+            data: e.results[0]
+          })
+        }
+      )
+  }
+  render() {
+    const { loading, data } = this.state;
+
+    if (loading)
+      return (<div className="wrap loading">
+        <div className="user">
+          <div className="picture"></div>
+          <div className="user_data">
+            <div className="username"></div>
+            <div className="email"></div>
+          </div>
+        </div>
+      </div>);
+
+    return (<div className="wrap">
+      <div className="user">
+        <div className="picture">
+          <img src={data.picture.large} alt={data.name.first} />
+        </div>
+        <div className="user_data">
+          <div className="username">
+            {data.name.first} {data.name.last}
+          </div>
+          <div className="email">{data.email}</div>
+        </div>
+      </div>
+    </div>)
+
+  }
 }
 
 export default App;
